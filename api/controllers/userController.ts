0b1af949d1b5f9826460db5296def8c5a0fbffa0
DerickMghanga <derickmghanga3329@gmail.com>
2024-04-23 12:40:53 +0300

    user routes and controllers

---
+++ api/controllers/userController.ts
@@ -0,0 +1,5 @@
+const signUpUser = async(req:any, res:any) => {
+    res.send("Sign up successfully!")
+}
+
+export {signUpUser}
--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,26 @@
 import express from 'express'
 import "dotenv/config";
 import connectDb from './db/connectDB';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import userRoutes from './routes/userRoutes'
 
 connectDb()
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+
+//Routes
+app.use('/api/users', userRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
