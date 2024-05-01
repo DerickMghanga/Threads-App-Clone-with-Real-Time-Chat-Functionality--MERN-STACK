68a3438054831c494a5eeae4dc52cba2f99e27b5
DerickMghanga <derickmghanga3329@gmail.com>
2024-05-01 16:10:35 +0300

    create post done

--- api/controllers/postController.ts
+++ api/controllers/postController.ts
@@ -1,5 +1,37 @@
-const createPost = (req:any, res:any) => {
+import Post from "../models/postModel"
+import User from "../models/userModel"
 
+
+//CREATE NEW POST
+const createPost = async (req: any, res: any) => {
+    try {
+        const { postedBy, text, img } = req.body
+
+        if (!postedBy || !text) {  //You cant post an empty post
+            return res.status(400).json({ message: "Text and postedBy details are required!" })
+        }
+
+        const user: any = await User.findById(postedBy)
+        if (!user) return res.status(404).json({ message: "User not found!" })
+
+        if (user._id.toString() !== req.user._id.toString()) { // you can ONLY post your own post >> req.user from protectRoute.ts
+            return res.status(400).json({ message: 'Unauthorized to create post!' })
+        }
+
+        const maxLength = 500
+        if (text.length > maxLength) {
+            return res.status(400).json({ message: `Text MUST be less than ${maxLength} characters!` })
+        }
+
+        const newPost = new Post({ postedBy, text, img }) // create new post object
+        await newPost.save()  //save to DB
+
+        res.status(201).json({ message: "New Post created successfully!", newPost })
+
+    } catch (err: any) {
+        res.status(500).json({ message: err.message })
+        console.log("Error on Create Post: ", err.message)
+    }
 }
 
-export {createPost}
+export { createPost }
--- api/routes/postRoutes.ts
+++ api/routes/postRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import { createPost } from "../controllers/postController"
+import protectRoute from "../middlewares/protectRoute"
 
 const router = express.Router()
 
-router.post('/create', createPost)
+router.post('/create', protectRoute, createPost)
 
 export default router
