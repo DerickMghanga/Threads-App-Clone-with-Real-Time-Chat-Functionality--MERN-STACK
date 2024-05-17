d29db708c540c89012391c12298b3c38d1196552
DerickMghanga <derickmghanga3329@gmail.com>
2024-05-17 15:05:24 +0300

    done feed

--- api/controllers/postController.ts
+++ api/controllers/postController.ts
@@ -6,16 +6,16 @@ import User from "../models/userModel"
 const getFeedPosts = async (req: any, res: any) => {
     try {
         const userId = req.user._id;
-		const user = await User.findById(userId);
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
 
-		const following = user.following;
+        const following = user.following;
 
-		const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+        const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
 
-		res.status(200).json(feedPosts);
+        res.status(200).json({ feedPosts });
 
     } catch (err: any) {
         res.status(500).json({ message: err.message })
