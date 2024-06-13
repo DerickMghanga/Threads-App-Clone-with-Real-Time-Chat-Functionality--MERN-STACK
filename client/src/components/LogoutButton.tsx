a27806470a8e2a970b58f81c24173cc46540249a
DerickMghanga <derickmghanga3329@gmail.com>
2024-06-13 14:19:54 +0300

    logout and custom hooks

--- client/src/components/LogoutButton.tsx
+++ client/src/components/LogoutButton.tsx
@@ -1,8 +1,42 @@
-import { Button } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
+import { useSetRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
 
 export default function LogoutButton() {
-    const handleLogOut = async () => {
 
+    const toast = useToast()
+
+    const setUser = useSetRecoilState(userAtom)
+
+    const handleLogOut = async () => {
+        try {
+            //fetch
+            const res = await fetch('/api/users/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const data = await res.json()
+            console.log(data)
+
+            if (data.error) {
+                toast({
+                    title: "Error",
+                    description: "LogOut failed",
+                    status: 'error',
+                    duration: 2000,
+                    isClosable: true
+                })
+                return
+            }
+
+            localStorage.removeItem('user-threads')
+            setUser(null)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <Button
---
+++ client/src/hooks/useShowToast.tsx
@@ -0,0 +1,8 @@
+
+export default function useShowToast() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
